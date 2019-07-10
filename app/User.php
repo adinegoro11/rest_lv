2a0bee84b7d73c6911700a9f6bb466b40699578b
erlangga <angga@cls-indo.com>
2019-07-10 22:09:21 +0700

    create migration and running db:seed

--- app/User.php
+++ app/User.php
@@ -3,12 +3,19 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    const VERIFIED_USER = '1';
+    const UNVERIFIED_USER = '0';
+
+    const ADMIN_USER = 'true';
+    const REGULAR_USER = 'false';
+
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +23,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','verified','verification_token','admin'
     ];
 
     /**
@@ -25,7 +32,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token','verification_token'
     ];
 
     /**
@@ -36,4 +43,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isVerified()
+    {
+        return $this->verified == User::VERIFIED_USER;
+    }
+
+    public function isAdmin()
+    {
+        return $this->admin == User::ADMIN_USER;
+    }
+
+    public function generateVerificationCode()
+    {
+        return str_random(40);
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\User;
+use App\Product;
+use App\Category;
+use App\Transaction;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate();
+
+        $users_qty = 200;
+        $categories_qty = 30;
+        $products_qty = 1000;
+        $transactions_qty = 1000;
+
+        factory(User::class, $users_qty)->create();
+        factory(Category::class, $categories_qty)->create();
+        factory(Product::class, $products_qty)->create()->each(
+            function ($product){
+                $categories = Category::all()->random(mt_rand(1,5))->pluck('id');
+
+                $product->categories()->attach($categories);
+            });
+        factory(Transaction::class, $transactions_qty)->create();
+
     }
 }
