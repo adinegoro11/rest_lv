b5bfeebcea7280e3ce09a2289fec69d81b5054f0
adinegoro11 <adinegoro11@gmail.com>
2019-07-12 23:53:49 +0700

    Implementation for show all sellers and seller details

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -8,24 +8,12 @@ use App\Http\Controllers\Controller;
 
 class BuyerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $buyers = Buyer::has('transactions')->get();
         return response()->json(['data' =>$buyers], 200);
     }
 
-  
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $buyer = Buyer::has('transactions')->findOrFail($id);
